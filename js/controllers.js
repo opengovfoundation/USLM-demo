cb7337c05da6c8d385bf2a5afd83271d220e981c
Chris Birk <cmbirk@gmail.com>
2014-10-30 00:40:13 -0400

    upload multiple files as tabs

--- js/controllers.js
+++ js/controllers.js
@@ -12,24 +12,21 @@ angular.module('uslmTest.controllers', [])
       $scope.onFileSelect = function ($files, $event) {
         $scope.input = $event.target;
 
-        if ($files.length > 1) {
-          growl.addWarnMessage('We can only handle one file at a time!');
-          return false;
-        }
-
-        $scope.upload = $upload.upload({
-          url: 'api',
-          file: $files
-        }).progress(function (event) {
-          console.log('percent: ' + parseInt(100.0 * event.loaded / event.total));
-        }).success(function (data) {
-          $scope.bills.push(data);
-        }).error( function (data) {
-          angular.forEach(data.messages, 
-            function (message) {
-              growl.addErrorMessage(message.text);
-            });
-          console.error(data);
+        angular.forEach($files, function (file) {
+          $scope.upload = $upload.upload({
+            url: 'api',
+            file: file
+          }).progress(function (event) {
+            console.log('percent: ' + parseInt(100.0 * event.loaded / event.total));
+          }).success(function (data) {
+            $scope.bills.push(data);
+          }).error( function (data) {
+            angular.forEach(data.messages, 
+              function (message) {
+                growl.addErrorMessage(message.text);
+              });
+            console.error(data);
+          });
         });
       };
 
@@ -41,6 +38,11 @@ angular.module('uslmTest.controllers', [])
         var input = $($scope.input);
         input.replaceWith(input = input.clone(true));
         $scope.input = null;
+      };
+
+      $scope.close = function (bill) {
+        var index = $scope.bills.indexOf(bill);
+        $scope.bills.splice(index, 1);
       };
 
     }]);
